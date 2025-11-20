dcfb90ef1e18fba8fbefedf12f30a4d2e28cdf0d
Senthamilselvi <senthamil727@gmail.com>
2025-11-20 09:22:03 +0530

    main.java

---
+++ main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n--- Student Management System ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Students");
+            System.out.println("3. Exit");
+            System.out.print("Choose option: ");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Roll No: ");
+                    String roll = sc.nextLine();
+
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter Age: ");
+                    int age = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.print("Enter Department: ");
+                    String dept = sc.nextLine();
+
+                    Student s = new Student(roll, name, age, dept);
+                    StudentManagementSystem.addStudent(s);
+                    break;
+
+                case 2:
+                    StudentManagementSystem.viewStudents();
+                    break;
+
+                case 3:
+                    System.out.println("Exiting...");
+                    return;
+
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        }
+    }
+}
